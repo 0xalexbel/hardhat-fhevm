0862ebf77fe6a791bfe54a015a0da5ebdfd33cec
Alexandre BELHOSTE <alexandre.belhoste@gmail.com>
2024-09-25 21:52:13 +0200

    fix

--- forge/fhevm.sol
+++ forge/fhevm.sol
@@ -27,8 +27,8 @@ library fhevm {
     uint8 internal constant eaddress_size_t = 20;
     uint8 internal constant euint256_size_t = 32;
     uint8 internal constant ebytes64_size_t = 64;
-    uint8 internal constant ebytes128_size_t = 128;
-    uint8 internal constant ebytes256_size_t = 256;
+    // uint8 internal constant ebytes128_size_t = 128;
+    // uint8 internal constant ebytes256_size_t = 256;
 
     Vm private constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
@@ -48,34 +48,30 @@ library fhevm {
     }
 
     function computeProofBool(bool value, bytes32 random) public pure returns (bytes memory result) {
-      return _computeProof(value, ebool_size_t, Common.ebool_t, random);
+      return _computeProof(value ? 1 : 0, ebool_size_t, Common.ebool_t, random);
     }
 
-    function computeProofU4(uint4 value, bytes32 random) public pure returns (bytes memory result) {
-      return _computeProof(value, euint4_size_t, Common.euint4_t, random);
+    function computeProofU4(uint8 value, bytes32 random) public pure returns (bytes memory result) {
+      return _computeProof(uint64(value), euint4_size_t, Common.euint4_t, random);
     }
 
     function computeProofU8(uint8 value, bytes32 random) public pure returns (bytes memory result) {
-      return _computeProof(value, euint8_size_t, Common.euint8_t, random);
+      return _computeProof(uint64(value), euint8_size_t, Common.euint8_t, random);
     }
 
     function computeProofU16(uint16 value, bytes32 random) public pure returns (bytes memory result) {
-      return _computeProof(value, euint16_size_t, Common.euint16_t, random);
+      return _computeProof(uint64(value), euint16_size_t, Common.euint16_t, random);
     }
 
     function computeProofU32(uint32 value, bytes32 random) public pure returns (bytes memory result) {
-      return _computeProof(value, euint32_size_t, Common.euint32_t, random);
+      return _computeProof(uint64(value), euint32_size_t, Common.euint32_t, random);
     }
 
     function computeProofU64(uint64 value, bytes32 random) public pure returns (bytes memory result) {
       return _computeProof(value, euint64_size_t, Common.euint64_t, random);
     }
 
-    function computeProofAddress(address value, bytes32 random) public pure returns (bytes memory result) {
-      return _computeProof(value, eaddress_size_t, Common.euint160_t, random);
-    }
-
-    function _computeProof(uint32 value, uint8 sizeT, uint8 typeT, bytes32 random) private pure returns (bytes memory result) {
+    function _computeProof(uint64 value, uint8 sizeT, uint8 typeT, bytes32 random) private pure returns (bytes memory result) {
       bytes memory pad = new bytes(53 - 1 - sizeT - 32);
       result = bytes.concat(bytes1(typeT), bytes8(value), random, pad);
     }
@@ -84,4 +80,8 @@ library fhevm {
       bytes32 handleNoIndex = keccak256(abi.encodePacked(proofHash, uint8(index)));
       result = (handleNoIndex & 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffff000000) | (bytes32(uint256(index)) << 16) | (bytes32(uint256(hType)) << 8);
     }
+
+    // function decrypt64(euint64 value) public view returns (uint64 result){
+    //   executor.db(euint64.unwrap(value))
+    // }
 }
